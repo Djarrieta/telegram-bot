0b5da88a27a0b5ae47619ded40af81f8a67c12d6
Dario Arrieta <arrieta.dario@hotmail.com>
2024-03-29 10:43:38 -0500

    main---> add dockerfile

--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,21 @@
 import { Telegraf } from "telegraf";
 import { message } from "telegraf/filters";
 import * as schema from "./schema";
-import { drizzle } from "drizzle-orm/bun-sqlite";
-import { Database } from "bun:sqlite";
-import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
-
 import { db } from "./db";
 
-// await db.insert(schema.movies).values([
-// 	{
-// 		title: "The Matrix",
-// 		releaseYear: 1999,
-// 	},
-// 	{
-// 		title: "The Matrix Reloaded",
-// 		releaseYear: 2003,
-// 	},
-// 	{
-// 		title: "The Matrix Revolutions",
-// 		releaseYear: 2003,
-// 	},
-// ]);
+const bot = new Telegraf(process.env.BOT_TOKEN || "");
 
-// console.log(`Seeding complete.`);
+bot.on(message("text"), async (ctx) => {
+	await db.insert(schema.movies).values([
+		{
+			title: ctx.message.text,
+			releaseYear: 0,
+		},
+	]);
 
-const result = await db.select().from(schema.movies);
-console.log(result);
+	const result = await db.select().from(schema.movies);
+	console.log(result);
+	await ctx.reply(`${ctx.message.text}`);
+});
 
-// const bot = new Telegraf(process.env.BOT_TOKEN || "");
-
-// bot.on(message("text"), async (ctx) => {
-// 	await ctx.reply(`Hello ${ctx.message.from.first_name}`);
-// });
-
-// bot.on(message("document"), async (ctx) => {
-// 	await ctx.reply(`Hello ${ctx.message.from.first_name}`);
-// });
-
-// bot.launch();
+bot.launch();
